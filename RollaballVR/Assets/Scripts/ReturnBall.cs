2beadeba2d1f0620f01c7008d962b1d98c8b9450
Brady Johnson <bradyjohnson84@gmail.com>
2022-02-01 16:57:50 -0600

    Add custom returnball script

---
+++ RollaballVR/Assets/Scripts/ReturnBall.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class ReturnBall : MonoBehaviour
+    {
+        // Adjust the speed for the application.
+        public float speed = 1.0f;
+        private Transform target;
+        public GameObject player;
+        private Interactable interactable;
+
+
+        void Start()
+        {
+            interactable = GetComponent<Interactable>();
+        }
+
+        void Update()
+        {
+            if(interactable != null && interactable.attachedToHand != null)
+            {
+                transform.position = transform.position;
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed);
+            }
+            
+        }
+    }
+}
